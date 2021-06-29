46f2d40bb28c2fa336add685b2cce07affd81acd
Denis Zapara <dzapara24@gmail.com>
2021-06-29 22:52:14 +0300

    добавляет готовую дз

--- src/index.js
+++ src/index.js
@@ -1,4 +1,55 @@
+import menuData from './menu.json';
+import foodCardMarkup from './templates/food-card.hbs';
 import './sass/main.scss';
-import templates from './templates/template.hbs';
 
-console.log(templates);
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+  localStgKey: 'Theme',
+};
+
+const foodMenuList = document.querySelector('.js-menu');
+const themeSwichCheckbox = document.querySelector('#theme-switch-toggle')
+
+foodMenuList.innerHTML = foodCardMarkup(menuData);
+
+themeSwichCheckbox.addEventListener('change', onCheckboxChecked)
+
+const currentValueThemeInLocalStg = localStorage.getItem(Theme.localStgKey)
+checkCurrentTheme(currentValueThemeInLocalStg)
+function checkCurrentTheme(currentTheme) {
+  if (currentTheme !== Theme.DARK) {
+    return
+  }
+
+  document.body.classList.add(Theme.DARK)
+  themeSwichCheckbox.checked = true;
+}
+
+function onCheckboxChecked(evt) {
+  const checkBoxValue = evt.target.checked;
+
+  if (checkBoxValue) {
+    setLightTheme();
+
+    const currentTheme = document.body.classList.value;
+    localStorage.setItem('Theme', currentTheme);
+  }
+
+  else {
+    setDarkTheme();
+
+    const currentTheme = document.body.classList.value;
+    localStorage.setItem('Theme', currentTheme);
+  }
+}
+
+function setLightTheme() {
+  document.body.classList.remove(Theme.LIGHT);
+  document.body.classList.add(Theme.DARK);
+}
+
+function setDarkTheme() {
+  document.body.classList.remove(Theme.DARK);
+  document.body.classList.add(Theme.LIGHT);
+}
